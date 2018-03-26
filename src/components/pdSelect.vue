d974b2769a248c2f3f51aceb986670e4a47e46d3
yilianyoumeng <1329395802@qq.com>
2018-03-26 23:05:58 +0800

    vue-linkage

--- src/components/pdSelect.vue
+++ src/components/pdSelect.vue
@@ -1,65 +1,71 @@
 <template>
-  <div class="formBox">
-    <div class="input_box" >
-      <div class="label_div">选择原因</div>
-      <select-normal :is-select-item="isSelectItem"  :placeholder="selectlaceholder" v-on:click="isSelectItem=true" v-on:changeSelectItem="selectItemFn" v-on:isSelectItemShow="isSelectItem=false" :select-item-arr="selectItemArr"></select-normal>
-    </div>
-    
+  <div class="mask" v-if="areaShow">
+      <select-box style="position: fixed;bottom: 0;width: 100%">
+        <div class="tit">
+          <div class="close" @click="closeArea()">确定</div>
+        </div>
+        <div style="display: flex;">
+          <select-item listName='provinceName' :listData="provinceList" v-model="province" v-on:changeVal="changeValFn"></select-item> 
+          <select-item listName='areaName' :listData="cityList" v-model="city" v-on:changeVal="changeValFn2" :toBegin="toBegin"></select-item>
+        </div>
+      </select-box>
   </div>
 </template>
 <script>
-  import selectNormal from './selectNormal.vue'
+  import selectBox from './selectBox.vue'
+  import selectItem from './selectItem.vue'
   export default{
     name: 'pdIosSelect',
     data () {
       return {
-        selectItemArr:[//下拉选择框数据
-          {name:'家庭购物消费',id:'shop'},
-          {name:'旅游娱乐',id:'travel'},
-          {name:'教育',id:'education'},
-          {name:'其他',id:'other'}
-        ],
-        isSelectItem:false,//下拉选择框是否显示
-			  redeemReason:'',//选中的退出原因
-			  selectlaceholder:'请选择',
+        cityList:Array.from({length: 12}, (value, index) => 1 + index),
+        provinceList:[{areaName}],
+        province:'1',
+        city:'',
+        toBegin:false,//城市初始化
+        areaShow:true
       }
     },
     components: {
-      selectNormal
+      selectBox,selectItem
+    },
+    created(){
+      this.test();
     },
     methods:{
-      selectItemFn:function(val){
-        console.log('下拉框父');
-        console.log(val);
-        this.redeemReason=val;
-        this.isSelectItem=false;
-        this.selectlaceholder='';
+      changeValFn:function(data){
+				var that=this;
+				that.toBegin=true;
+				console.log('省份测试:');
+				console.log(data);
+				// that.provinceCode=data[0];
+				//var i=data[1];
+				// that.cityList=that.allAddressList[i].areaInfoVOList;
+				// that.cityCode=that.cityList[0].code;
+				// that.city=that.cityList[0].areaName;
+				
+			},
+			changeValFn2:function(data){
+				var that=this;
+				that.toBegin=false;
+				console.log('城市测试:');
+				console.log(data);
+				// that.cityCode=data[0];
+				
       },
+      //关闭弹出层
+			closeArea:function(){
+				this.areaShow=false;
+			},
+      test:function(){
+        console.log(this.cityList);
+        console.log(this.provinceList);
+      }
     }
   }
 </script>
 <style lang="less" scoped>
 @import '../assets.less';
-.formBox{
-  background-color: #fff;
 
-  .input_box{
-    width: 668*@px1;
-    margin:auto;
-    background: #fff;
-    font-size: 0;
-    .label_div{
-      color: #666;
-      width: 200*@px1;
-      display: inline-block;
-      font-size: 36*@px1;
-      line-height: 3;
-    }
-    .selectNormal{
-      width:6rem;
-      font-size: 36*@px1;
-    }
-  }
-}
 </style>
 
